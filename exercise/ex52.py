115b2f62170fec50239c3bc6e9de6da87b82db6d
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-06-18 00:12:19 +0900

    2019/06/18 Solved the problem

---
+++ exercise/ex52.py
@@ -0,0 +1,25 @@
+// exercise 5.2
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done" : break
+
+    try:
+        fnum = int(num) # number인지 감지
+    except:
+        print("Invalid input")
+        continue
+
+    if largest is None:
+        largest = fnum
+    elif fnum > largest:
+        largest = fnum
+
+    if smallest is None:
+        smallest = fnum
+    elif fnum < smallest:
+        smallest = fnum
+
+print("Maximum is", largest)
+print("Minimum is", smallest)
