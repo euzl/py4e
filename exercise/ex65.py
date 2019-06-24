44feaa9c37eedcb41ff56515ffab36541f52f99a
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-06-24 14:54:46 +0900

    2019/06/24 Solved the problem

---
+++ exercise/ex65.py
@@ -0,0 +1,7 @@
+# 연습문제 6.5
+text = "X-DSPAM-Confidence:    0.8475" # 주어진 숫자만 추출할 문장
+fnd = ':'
+tnum = text[text.find(fnd)+1:] # ':' 다음 문자열부터 자른다.
+
+output = float(tnum.strip()) # 공백제거
+print(output)
