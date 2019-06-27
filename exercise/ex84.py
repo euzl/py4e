6349fdf7baa97b143a63381f25a0d805e27526b5
Yujin Lee (이유진) <euzl@khu.ac.kr>
2019-06-27 16:39:21 +0900

    2019/06/27 Solved the problem

--- exercise/ex84.py
+++ exercise/ex84.py
@@ -3,7 +3,7 @@ fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
 for line in fh:
-    line.rstrip() # 개행문자 제거
+    line = line.rstrip() # 개행문자 제거
     word = line.split()
     for nw in word:
         if nw not in lst:
